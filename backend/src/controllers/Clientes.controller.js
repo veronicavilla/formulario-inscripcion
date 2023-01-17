a0a4399ed2f6526f990c2730216aa1574e619142
veronicavilla <verito97montoya@gmail.com>
2023-01-17 01:11:04 -0500

    segundo intento con algunos cambios

--- backend/src/controllers/Clientes.controller.js
+++ backend/src/controllers/Clientes.controller.js
@@ -3,7 +3,7 @@ import { getConnection } from "./../database/database";
 const getClientes = async (req, res) => {
   const connection = await getConnection();
   const result = await connection.query(
-    "SELECT Número Identificación, Nombres, Apellidos, Dirección FROM Clientes");
+    "SELECT numero_identificacion, nombres, apellidos, direccion FROM Clientes");
   console.log(result);
   res.json(result)
 };
